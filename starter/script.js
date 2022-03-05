01687cf0edbd86a595d05d9a4095f4342fcd4b5f
That Random <andrewwoan@gmail.com>
2022-03-05 08:08:55 -0500

    Updated Starter Files

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,1 @@
-// Utility Function(s)
 
-const lerp = (current, target, factor) => {
-    let holder = current * (1 - factor) + target * factor;
-    holder = parseFloat(holder).toFixed(2);
-    return holder;
-};
